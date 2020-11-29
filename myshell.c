01e39f743d51febc7281f8de7111aed05ff79b60
StupenkovValery <stupenkov.val@yandex.ru>
2020-11-29 12:34:15 +0300

    o

--- myshell.c
+++ myshell.c
@@ -8,6 +8,7 @@
 #include <errno.h>
 #include <limits.h>
 #include <fcntl.h>
+#include <wait.h>
 
 void directory(void)
 {
@@ -22,6 +23,7 @@ int processing(char **words,int j)
 {
     char *home=NULL;
     int i,a,pid;
+    int flag=0;
     if (words!=NULL){
         if (strcmp(words[0],"exit")==0)
             return 2;
@@ -38,47 +40,62 @@ int processing(char **words,int j)
         else{
             /*Создание нoвого процесса, вызов программы*/
             if ((pid=fork())==0){
-                /*перед execvp если есть > or >> то перенаправление вывода */
+                /*перед execvp если есть > or >> or < то перенаправление вывода, ввода */
+                /*нужно добавить удаление символов >,<,>> из массива слов!!!!*/
                 for (i=0;i<j-1;i++){
-                    if (strcmp(words[i],">")==0){
-                        if (words[i+1]==NULL){
-                            fprintf(stderr,"Ошибка в перенаправлении");
-                            break;
+                    if (flag==0){
+                        if (strcmp(words[i],">")==0){
+                            if (words[i+1]==NULL){
+                                fprintf(stderr,"Ошибка в перенаправлении");
+                                break;
+                            }
+                            int fd=open(words[i+1],O_WRONLY | O_CREAT | O_TRUNC,0666);
+                            if (fd<0){
+                                fprintf(stderr,"Ошибка при открытии/создании файла");
+                                break;
+                            } 
+                            dup2(fd,STDOUT_FILENO);
+                            close(fd);  
+                            flag=1;
                         }
-                        int fd=open(words[i+1],O_WRONLY | O_CREAT | O_TRUNC,0666);
-                        if (fd<0){
-                            fprintf(stderr,"Ошибка при открытии/создании файла");
-                            break;
+                        else if (strcmp(words[i],"<")==0){
+                            if (words[i+1]==NULL){
+                                fprintf(stderr,"Ошибка в перенаправлении");
+                                break;
+                            }
+                            int fd=open(words[i+1], O_RDONLY,0);
+                            if (fd<0){
+                                fprintf(stderr,"Ошибка при открытии/создании файла");
+                                break;
+                            }
+                            dup2(fd,STDIN_FILENO);
+                            close(fd); 
+                            flag=1;        
                         } 
-                        dup2(fd,STDOUT_FILENO);
-                        close(fd);  
+                        else if (strcmp(words[i],">>")==0){
+                            if (words[i+1]==NULL){
+                                fprintf(stderr,"Ошибка в перенаправлении");
+                                break;
+                            }
+                            int fd=open(words[i+1], O_WRONLY | O_CREAT | O_APPEND,0666);  
+                            if (fd<0){
+                                fprintf(stderr,"Ошибка при открытии/создании файла");
+                                break;
+                            }
+                            dup2(fd,STDOUT_FILENO);
+                            close(fd);
+                            flag=1;          
+                        } 
                     }
-                    if (strcmp(words[i],"<")==0){
-                        if (words[i+1]==NULL){
-                            fprintf(stderr,"Ошибка в перенаправлении");
-                            break;
-                        }
-                        int fd=open(words[i+1], O_RDONLY,0);
-                        if (fd<0){
-                            fprintf(stderr,"Ошибка при открытии/создании файла");
-                            break;
-                        }
-                        dup2(fd,STDIN_FILENO);
-                        close(fd);         
-                    } 
-                    else if (strcmp(words[i],">>")==0){
-                        if (words[i+1]==NULL){
-                            fprintf(stderr,"Ошибка в перенаправлении");
-                            break;
-                        }
-                        int fd=open(words[i+1], O_WRONLY | O_CREAT | O_APPEND,0666);  
-                        if (fd<0){
-                            fprintf(stderr,"Ошибка при открытии/создании файла");
-                            break;
-                        }
-                        dup2(fd,STDOUT_FILENO);
-                        close(fd);          
-                    }   
+                    if (flag==1){
+                        free(words[i+1]);
+                        words[i+1]=NULL;
+                        free(words[i]);
+                        words[i]=NULL;
+                        flag++;
+                    }
+                    else if (flag==2)
+                        flag=0;
                 }
                 
                 execvp(words[0],words);
@@ -87,7 +104,7 @@ int processing(char **words,int j)
             }
             else if (pid<0){
                 perror("Fork's mistake");
-                exit(1);
+                return 3;
             }
             else 
                 wait(0); 
@@ -96,17 +113,84 @@ int processing(char **words,int j)
     return 0;
 }
 
+int PipeN(char **words,int j)
+{
+    int fd[2],i=0,a=0,b=0,pid;
+    char **mas=NULL;
+
+    while (words[i]!=NULL){
+        if (strcmp(words[i],"|")==0){
+            mas=realloc(mas,sizeof(char**)*(a+1));
+            mas[a]=NULL;
+            if (pipe(fd)<0){
+                perror("Pipe's error");
+                exit(1);
+            }      
+            if ((pid=fork())==0){
+                if ((i+1)!=j)
+                    dup2(fd[1],1);
+                close(fd[0]);
+                close(fd[1]);
+                execvp(mas[0],mas);
+                perror("Execvp error");
+                return 4;    
+            }
+            else if (pid<0){
+                perror("Fork error");
+                return 5;
+            }  
+            dup2(fd[0],0);
+            close(fd[0]);
+            close(fd[1]);
+            for (b=0;b<a;b++){
+                free(mas[b]);
+                mas[b]=NULL;
+            } 
+            free(mas);
+            mas=NULL;
+            a=0; 
+            i++;  
+        }
+        else{
+            mas=realloc(mas,sizeof(char**)*(a+1));
+            mas[a]=strdup(words[i]);
+            a++;  
+            i++;  
+        }     
+    }
+    if ((pid=fork())==0){
+        mas=realloc(mas,sizeof(char**)*(a+1));
+        mas[a]=NULL;
+        execvp(mas[0],mas);
+        perror("Execvp error");
+        return 4;    
+    }
+    else if (pid<0){
+        perror("Fork error");
+        return 5;
+    } 
+    else 
+        wait(NULL); 
+    while (wait(NULL)!=-1);
+    for (b=0;b<a;b++){
+        free(mas[b]);
+        mas[b]=NULL;
+    } 
+    free(mas);
+    return 0;        
+}
+
 int main()
 {
     char **words=NULL;
     char *w=NULL;
     char *home=NULL;
     char c;
-    int prob=0,kav=0,i=0,j=0,stat,size=1,vvod=0;;
+    int prob=0,kav=0,i=0,j=0,stat,size=1,vvod=0,a,conv=0;
 
     directory();   
     while ((c=getchar())!=EOF){
-        if (c=='\n' && kav==0){
+        if (c=='\n' && kav==0){ /*запись последнего слова в массив, вызов обработки строки*/
             if (w!=NULL){
                 w[i]='\0';
                 words=realloc(words,sizeof(char**)*(j+1));
@@ -124,10 +208,33 @@ int main()
             words=realloc(words,sizeof(char**)*(j+1));
             words[j]=NULL;  
             j++;
-            stat=processing(words,j);
+            
+            for (a=0;a<j-1;a++){
+                if (strcmp(words[a],"|")==0)
+                    conv=1;    
+            }
+            if (conv==1){
+                if (fork()==0){
+                    PipeN(words,j);
+                    for (i=0;i<j;i++){
+                        free(words[i]);
+                        words[i]=NULL;
+                    }
+                    free(words);
+                    words=NULL;
+                    i=0;
+                    return 0;
+                }
+                else 
+                    wait(NULL);
+                conv=0;
+                stat=0;
+            }
+            else
+                stat=processing(words,j);
             for (i=0;i<j;i++){
                 free(words[i]);
-                words[i]=0;
+                words[i]=NULL;
             }
             i=0;
             free(words);
@@ -137,10 +244,12 @@ int main()
                 return 1;
             else if (stat==2)
                 return 0;
+            else if (stat==3)
+                return 2;
             directory();
             prob=0;
         }
-        else if ((isspace(c)||(c=='>')||(c=='<')) && kav==0){
+        else if ((isspace(c)||(c=='>')||(c=='<')||(c=='|')) && kav==0){
             if (prob==0){
                 /*добавление слова в массив*/
                 if (w!=NULL){
@@ -158,33 +267,33 @@ int main()
                     size=1;
                 }   
             }
-            if (c=='<'){
-                    /*обработка и добавление <*/
-                    if ((i+1)==size){
-                        size=size*2+1;
-                        w=realloc(w,size);
-                        if (w==NULL){
-                            fprintf(stderr,"Error\n");
-                            exit(1);
-                        }
-                    }
-                    w[i]=c;
-                    i++;
-                    words=realloc(words,sizeof(char**)*(j+1));
-                    if (words==NULL){
+            if ((c=='<')||(c=='|')){
+            /*обработка и добавление < или |*/
+                if ((i+1)==size){
+                    size=size*2+1;
+                    w=realloc(w,size);
+                    if (w==NULL){
                         fprintf(stderr,"Error\n");
                         exit(1);
                     }
-                    w[i]='\0';
-                    i++;
-                    words[j]=strdup(w);
-                    j++;
-                    free(w);
-                    w=NULL;
-                    i=0;
-                    size=1;    
                 }
-                else if (c=='>'){ 
+                w[i]=c;
+                i++;
+                words=realloc(words,sizeof(char**)*(j+1));
+                if (words==NULL){
+                    fprintf(stderr,"Error\n");
+                    exit(1);
+                }
+                w[i]='\0';
+                i++;
+                words[j]=strdup(w);
+                j++;
+                free(w);
+                w=NULL;
+                i=0;
+                size=1;    
+            }
+            else if (c=='>'){ 
                     /*Обработка и добавление >*/
                     if (vvod==0){
                         if ((i+1)==size){
@@ -257,7 +366,8 @@ int main()
                 i=0;
                 size=1; 
                 vvod=0;  
-            }    
+            }
+            /*добавление символов в слово*/    
             if ((i+1)==size){
                 size=size*2+1;
                 w=realloc(w,size);
@@ -271,7 +381,7 @@ int main()
             prob=0;     
         }
     }
-
+    /*Если конец файла, то надо дозаписать в массив слов и выполнить последнюю команду*/
     if (w!=NULL){
         w[i]='\0';
         words=realloc(words,sizeof(char**)*(j+1));
