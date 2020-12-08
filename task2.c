320949efb564ec0ec81c3990bf4e6cbb111b7200
StupenkovValery <stupenkov.val@yandex.ru>
2020-12-08 16:09:31 +0300

    Inter

--- task2.c
+++ task2.c
@@ -10,30 +10,21 @@
 int main(int argc, char **argv)
 {
     int in=open("text.txt",O_RDONLY,0);
-    int out=open("text.txt",O_WRONLY | O_TRUNC,0);
+    int out=open("text.txt",O_RDWR,0);
     if (in<0 || out<0){
         perror("Can't open file");
         return 1;
     }
     char buf[1024];
-    int i=0,kol=0,a=0;
-    read(in,buf,1024);
-    printf("%d",buf[0]);
-    while (buf[i]!=EOF){
-        if (buf[i]=='\n'){
-            if (kol==2){
-                write(out,buf,i);
-                a=a+i;
-                i=0;
-            }
-            kol=0;        
+    char *p;
+    int i=0,kol=0,a=0,n,nw;
+    while ((n=read(in,buf,1024))!=0){
+        p=buf;
+        while (n){
+            nw=write(out,p,n);
+            p=p+nw;
+            n=n-nw;
         }
-        else {
-            if (isdigit(buf[i])){
-                kol++;   
-            } 
-            i++;   
-        }            
     }
     ftruncate(out,a);
     close(in); 
