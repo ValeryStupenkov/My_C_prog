987acce1707a58aa3b5d3e4d4b0a1dfa8954a148
StupenkovValery <stupenkov.val@yandex.ru>
2020-12-08 15:00:25 +0300

    Itern

--- task1.c
+++ task1.c
@@ -1,7 +1,50 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include <wait.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char **argv)
 {
-
+    int pid,status;
+    int fd[2];
+    pipe(fd);
+    for (int i=1;i<=2;i++){
+        if ((pid=fork())==0){
+            dup2(fd[1],1);
+            close(fd[0]);
+            close(fd[1]);
+            execlp(argv[i],argv[i],NULL);
+            perror("Execlp error");
+            return 1;
+        }
+        else {
+            waitpid(pid,&status,0);    
+        }
+    }
+    if ((pid=fork())==0){
+        dup2(fd[0],0);
+        close(fd[0]);
+        close(fd[1]);
+        execlp(argv[3],argv[3],NULL);
+        perror("Execlp error");
+        return 1;    
+    } 
+    else {
+        waitpid(pid,&status,0);    
+    }
+    if (WIFEXITED(status)){
+        if ((pid=fork())==0){
+            dup2(fd[0],0);
+            close(fd[0]);
+            close(fd[1]);
+            execlp(argv[3],argv[3],NULL);
+            perror("Execlp error");
+            return 1;    
+        } 
+        else {
+            waitpid(pid,&status,0);    
+        }    
+    }
+    return 0;                
 }
