4c530007ca9ef3f697e4615e9b67a8174e5f3600
StupenkovValery <stupenkov.val@yandex.ru>
2020-12-08 17:02:49 +0300

    inter

--- task2.c
+++ task2.c
@@ -10,19 +10,21 @@
 int main(int argc, char **argv)
 {
     int fd=open(argv[1], O_RDWR,0777);
+    if (fd<0){
+        perror("No file");
+        exit(1);   
+    }   
     char c;
     char *p;
-    int i=0,kol=0,a=0,str=0,n=0;
+    int i=0,kol=0,a=0,str=1,n=0,end=0;
     while ((n=read(fd,&c,1))!=0){
-        i++;
-        str++;
         if (c=='\n'){
+            str++;
             if (kol==2){
                 i=i-str;
-			    while(read(fd, &c, 1)){
-				    lseek(fd, i+a, SEEK_SET);
+                lseek(fd, end, SEEK_SET);
+			    while(read(fd, &c, 1) && a!=i){
 				    write(fd, &c, 1);
-				    lseek(fd, i+str+a, SEEK_SET);
 				    a++;
 			    }
             a=0;
@@ -30,6 +32,7 @@ int main(int argc, char **argv)
             }
             kol=0;
             str=0;
+            end=1;
         }
         if (c!='\n'){
             if (isdigit(c)){
