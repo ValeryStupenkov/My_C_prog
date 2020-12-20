f5f3530fd4118ed5b27b37fe575684047516d681
StupenkovValery <stupenkov.val@yandex.ru>
2020-12-20 20:08:52 +0300

    бонусный этап

--- myshell.c
+++ myshell.c
@@ -12,8 +12,10 @@
 #include <signal.h>
 
 int conv=0,fon=0,fsec=0,forsec=0,fandsec=0;
+int fd1[2];
+int executeorder(char **, int );
 
-void directory(void)
+void directory(void) /*печать текущей директории*/
 {
     char buf[PATH_MAX];
     if (getcwd(buf,PATH_MAX)!=NULL)
@@ -22,7 +24,7 @@ void directory(void)
         perror("Ошибка в getcwd\n");
 }
 
-void ChangeInOut(char **words, int j)
+void ChangeInOut(char **words, int j) /*перенаправление ввода/вывода*/
 { /*перенаправляет ввод, вывод*/
     int i,flag=0;
     
@@ -84,7 +86,7 @@ void ChangeInOut(char **words, int j)
     return;   
 }
 
-void fonproc()
+void fonproc() /*контроль фоновых процессов*/
 {
     int status,pid;
     pid=waitpid(-1,&status,WNOHANG);
@@ -97,7 +99,7 @@ void fonproc()
     }
 }
 
-int PipeN(char **words,int j)
+int PipeN(char **words,int j) /*функция обеспечивающая работу конвейера*/
 {
     int fd[2],i=0,a=0,b=0,pid,status;
     char **mas=NULL;
@@ -181,7 +183,7 @@ int PipeN(char **words,int j)
 }
 
 
-int processing(char **words,int j)
+int processing(char **words,int j) /*функция обработки команды*/
 {
     char *home=NULL;
     int i,a,pid,status,stat=0;
@@ -259,11 +261,123 @@ int processing(char **words,int j)
     return WEXITSTATUS(status);
 }
 
-int executeorder(char **words,int j)
+int Pipescob(char **words,int j){
+    char **mas=NULL;
+    int fd[2],i=0,pid,status=0,a=0,stat=0,skob=0;
+    while (words[i]!=NULL){
+        if (strcmp(words[i],"|")==0 && skob==0){
+            mas=realloc(mas,sizeof(char**)*(a+1));
+            mas[a]=NULL;
+            if (pipe(fd)<0){
+                perror("Pipe error\n");
+                return -1;
+            }
+            if ((pid=fork())==0){
+                if ((i+1)!=j)
+                    dup2(fd[1],1);
+                close(fd[0]);
+                close(fd[1]);
+                status=executeorder(mas,a);
+                for (int b=0;b<a;b++){
+                    free(mas[b]);
+                    mas[b]=NULL;
+                }
+                free(mas);
+                mas=NULL;
+                return -1;
+            }
+            else if (pid<0)
+                return -1;
+            else {
+                dup2(fd[0],0);
+                close(fd[0]);
+                close(fd[1]);
+                for (int b=0;b<a;b++){
+                    free(mas[b]);
+                    mas[b]=NULL;
+                }
+                free(mas);
+                mas=NULL;
+                a=0;
+                i++;
+            }
+        }
+        else{ /*преобразовать добавление в мас так, чтобы то что внутри скобок было одной командой*/
+            if (strcmp(words[i],"(")==0)
+                skob=1;
+            else if (strcmp(words[i],")")==0)
+                skob=0;
+            mas=realloc(mas,sizeof(char**)*(a+1));
+            mas[a]=strdup(words[i]);
+            a++;
+            i++;
+        }
+    }
+    if (mas==NULL)
+        fprintf(stderr,"Error\n");
+    else{
+        if ((pid=fork())==0){
+            mas=realloc(mas,sizeof(char**)*(a+1));
+            mas[a]=NULL;
+            status=executeorder(mas,a); 
+            for (int b=0;b<a;b++){
+                free(mas[b]);
+                mas[b]=NULL;
+            }
+            free(mas);
+            mas=NULL;
+            a=0;   
+            return -1;
+        }
+        else if (pid<0)
+            return -1;
+        else
+            waitpid(pid,&stat,0);
+    }
+    while (wait(NULL)!=-1);
+    for (int b=0;b<a;b++){
+        free(mas[b]);
+        mas[b]=NULL;
+    }
+    free(mas);
+    return 0;
+}
+
+int executeorder(char **words,int j) /*просмотр строки, вызов функции обработки команды*/
 {
     char **comand=NULL;
-    int i=0,a=0,status=0,skob=0,other=0;
-    while (words[i]!=NULL){
+    int i=0,a=0,status=0,skob=0,other=0,pid,stat=0,conveer=0;
+    
+    for (i=0;i<j-1;i++){
+        if (strcmp(words[i],"|")==0 && (other==1 || strcmp(words[i+1],"(")==0)){
+            conveer=1;
+        }
+        else if (strcmp(words[i],")")==0){
+            if (skob==1) {
+                skob=0;
+                other=1;
+            }
+        }
+        else if (strcmp(words[i],"(")==0)
+            skob=1;
+        else if (other==1 && strcmp(words[i],"|")!=0)
+            other=0;
+    }   
+    if (conveer==1){
+        if ((pid=fork())==0){
+            status=Pipescob(words,j);
+            return 10;
+        }
+        else if (pid>0)
+            waitpid(pid,&stat,0);
+        else{
+            fprintf(stderr,"Fork error");
+            return -1;
+        }
+    }
+    skob=0;
+    i=0;
+    while (words[i]!=NULL && conveer==0){
         if (strcmp(words[i],"(")==0){
             skob=1;       
             i++;
@@ -276,6 +390,8 @@ int executeorder(char **words,int j)
                     comand[b]=NULL;
                 }      
                 free(comand);
+                comand=NULL;
+                a=0;
                 return 0;
             }
             else {
@@ -294,7 +410,7 @@ int executeorder(char **words,int j)
                     other=0;
                 }
                 else
-                    status=processing(comand,a);
+                status=processing(comand,a);         
             }
             for (int b=0;b<a;b++){
                 free(comand[b]);
@@ -326,7 +442,7 @@ int executeorder(char **words,int j)
             i++;      
         }    
     }
-    if (((fsec==0 && forsec==0 && fandsec==0) || (fsec==1) || (forsec==1 && status!=0) || (fandsec==1 && status==0))&&a!=0){
+    if (((fsec==0 && forsec==0 && fandsec==0) || (fsec==1) || (forsec==1 && status!=0) || (fandsec==1 && status==0))&& a!=0 && conveer==0){
         comand=realloc(comand,sizeof(char**)*(a+1));
         comand[a]=NULL;
         a++;
@@ -334,8 +450,8 @@ int executeorder(char **words,int j)
             status=executeorder(comand,a);
             other=0;
         }
-        else 
-            status=processing(comand,a);
+        else
+            status=processing(comand,a);       
         for (int b=0;b<a;b++){
             free(comand[b]);
             comand[b]=0;
@@ -359,6 +475,7 @@ int executeorder(char **words,int j)
     fsec=0;
     forsec=0;
     fandsec=0;
+    conveer=0;
     return status;        
 }
 
@@ -448,6 +565,22 @@ int main()
             }
             if ((c=='<')||(c==';')||(c=='(')||(c==')')||(c=='`')){
             /*обработка и добавление <,(,) или ;*/
+                if (vvod==1 || vvod1==1 || vvod2==1){
+                    if (w!=NULL){
+                        words=realloc(words,sizeof(char**)*(j+1));
+                        if (words==NULL){
+                            fprintf(stderr,"Error\n");
+                            exit(1);
+                        }
+                        w[i]='\0';
+                        words[j]=strdup(w);
+                        j++;
+                        free(w);
+                        w=NULL;
+                        i=0;
+                        size=1;
+                    }       
+                }
                 if ((i+1)==size){
                     size=size*2+1;
                     w=realloc(w,size);
@@ -470,7 +603,10 @@ int main()
                 free(w);
                 w=NULL;
                 i=0;
-                size=1;   
+                size=1; 
+                vvod=0;
+                vvod1=0;
+                vvod2=0;  
             }
             else if (c=='>'||c=='&'||c=='|'){ 
                     /*Обработка и добавление >, & or |*/
